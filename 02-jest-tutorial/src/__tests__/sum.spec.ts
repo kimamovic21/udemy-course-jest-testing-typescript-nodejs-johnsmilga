0f0c50f510963380d7de87a3a3909c3da5323069
Kerim Imamović <imamovic.kerim@gmail.com>
2025-10-02 12:47:23 +0200

    Jest syntax

--- 02-jest-tutorial/src/__tests__/sum.spec.ts
+++ 02-jest-tutorial/src/__tests__/sum.spec.ts
@@ -4,7 +4,13 @@ describe('sum function', () => {
   it('should add two numbers correctly', () => {
     expect(sum(1, 2)).toBe(3);
   });
-  it.only('should handle negative numbers', () => {
-    expect(sum(-1, -2)).toBe(-3);
+  // it.only('should handle negative numbers', () => {
+  //   expect(sum(-1, -2)).toBe(-3);
+  // });
+  it('should handle negative numbers', () => {
+      expect(sum(-1, -2)).toBe(-3);
+    });
+  it('should do nothing', () => {
+    console.log('empty test case');
   });
 }); 
