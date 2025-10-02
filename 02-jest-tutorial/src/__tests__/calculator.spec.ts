7683176aec3a5189a9d3fa95467fc77185c49780
Kerim Imamović <imamovic.kerim@gmail.com>
2025-10-02 14:59:47 +0200

    Jest hooks

--- 02-jest-tutorial/src/__tests__/calculator.spec.ts
+++ 02-jest-tutorial/src/__tests__/calculator.spec.ts
@@ -1,30 +1,31 @@
 import Calculator from '../Calculator';
 
 describe('Calculator', () => {
+  let sut: Calculator;
+
+  beforeEach(() => {
+    sut = new Calculator();
+  });
+
   it('should start with value 0', () => {
-    const calculator = new Calculator();
-    expect(calculator.value).toBe(0);
+    expect(sut.value).toBe(0);
   });
 
   it('should add a number to the current value', () => {
-    const calculator = new Calculator();
-    expect(calculator.add(5)).toBe(5);
+    expect(sut.add(5)).toBe(5);
   });
 
   it('should subtract a number from the current value', () => {
-    const calculator = new Calculator();
-    calculator.add(10);
-    expect(calculator.subtract(3)).toBe(7);
+    sut.add(10);
+    expect(sut.subtract(3)).toBe(7);
   });
 
   it('should return false when value is not positive', () => {
-    const calculator = new Calculator();
-    expect(calculator.isPositive()).not.toBeTruthy();
+    expect(sut.isPositive()).not.toBeTruthy();
   });
 
   it('should return true when value is positive', () => {
-    const calculator = new Calculator();
-    calculator.add(1);
-    expect(calculator.isPositive()).toBeTruthy();
+    sut.add(1);
+    expect(sut.isPositive()).toBeTruthy();
   });
 });
